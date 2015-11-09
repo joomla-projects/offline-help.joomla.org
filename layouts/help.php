f7bdc3541e2a5913d46e03633b945cc9c89ec196
Michael Babker <mbabker@flbab.com>
2015-11-09 09:42:14 -0500

    Add Google Tag Manager code

--- layouts/help.php
+++ layouts/help.php
@@ -31,6 +31,10 @@
         </style>
     </head>
     <body>
+        <!-- Google Tag Manager -->
+        <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-T742X9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T742X9');</script>
+        <!-- End Google Tag Manager -->
    		<div class="container">
             <div class="well text-center">
                 <img src="media/logo.png" alt="Joomla! Logo">
